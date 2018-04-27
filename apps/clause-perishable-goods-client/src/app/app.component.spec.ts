d9075efda28324cf7f4191708dc53ffb9898cb98
Matt Roberts <matt@clause.io>
2018-04-27 08:13:30 +0100

    Added basic Angular tests

--- apps/clause-perishable-goods-client/src/app/app.component.spec.ts
+++ apps/clause-perishable-goods-client/src/app/app.component.spec.ts
@@ -1,10 +1,21 @@
 import { TestBed, async } from '@angular/core/testing';
 import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
+import { ComposerPerishableGoodsService } from './composer.service';
+import { HttpClientModule } from '@angular/common/http';
+
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
+      imports: [
+        FormsModule,
+        HttpClientModule
+      ],
       declarations: [
         AppComponent
+      ],
+      providers: [
+        ComposerPerishableGoodsService
       ],
     }).compileComponents();
   }));
@@ -16,12 +27,12 @@ describe('AppComponent', () => {
   it(`should have as title 'app'`, async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('app');
+    expect(app.title).toEqual('Smart Legal Contracts with IBM Blockchain, by Clause');
   }));
   it('should render title in a h1 tag', async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to app!');
+    expect(compiled.querySelector('h3#deploy').textContent).toContain('Deploy your Smart Legal Contract');
   }));
 });
--- apps/clause-perishable-goods-client/src/app/composer.service.spec.ts
+++ apps/clause-perishable-goods-client/src/app/composer.service.spec.ts
@@ -1,10 +1,14 @@
 import { TestBed, inject } from '@angular/core/testing';
 
+import { HttpClientModule } from '@angular/common/http';
 import { ComposerPerishableGoodsService } from './composer.service';
 
 describe('ComposerPerishableGoodsService', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
+      imports: [
+        HttpClientModule
+      ],
       providers: [ComposerPerishableGoodsService]
     });
   });
